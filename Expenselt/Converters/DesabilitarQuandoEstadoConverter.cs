13edcc561cc70babe682d4397aac1ddc951cea6c
uira.peixoto <uira.peixoto@gmail.com>
2018-11-20 18:30:05 -0200

    Tentado

---
+++ Expenselt/Converters/DesabilitarQuandoEstadoConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Expenselt.Converters
+{
+    public class DesabilitarQuandoEstadoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!string.IsNullOrEmpty(value.ToString()) && value.ToString().ToLower().Equals("estado"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
